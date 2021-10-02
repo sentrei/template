c6997795cbd3ed57d09abb516a2b5c46e9811362
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-02 15:46:58 -0700

    feat: ini template repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { RecoilRoot } from "recoil";
+
+import "tailwindcss/tailwind.css";
+
+const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "3fff5b53524d4928bae2c465c1ac14f2"}'
+      />
+      <RecoilRoot>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
+  );
+};
+
+export default CustomApp;
